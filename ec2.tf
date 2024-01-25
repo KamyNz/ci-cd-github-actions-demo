21102940215e37b936077b76e8836c9abd01f37c
kamynz16 <kamynz16@gmail.com>
2024-01-25 18:03:36 -0500

    :construction_worker: Reformat .tf files and fix providers error

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "ccamp-github-actions" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Specify the AMI ID of the instance
-  instance_type = "t2.micro"               # Specify the instance type
+  ami           = "ami-0c55b159cbfafe1f0" # Specify the AMI ID of the instance
+  instance_type = "t2.micro"              # Specify the instance type
 
   tags = {
-    Name = "ccamp-github-actions"
-    Owner = var.owner
-    OTU = var.OTU
-    Class = var.Class
+    Name     = "ccamp-github-actions"
+    Owner    = var.owner
+    OTU      = var.OTU
+    Class    = var.Class
     bootcamp = var.bootcamp
 
   }
