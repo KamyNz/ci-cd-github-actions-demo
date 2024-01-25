c14520f1003c6175df2becede58f9c9033cb43bf
kamynz16 <kamynz16@gmail.com>
2024-01-25 17:57:02 -0500

    :construction_worker: Edit providers.tf to fix ci/cd with githubactions

--- providers.tf
+++ providers.tf
@@ -8,5 +8,5 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
+  #shared_credentials_files = ["~/.aws/credentials"]
 }
