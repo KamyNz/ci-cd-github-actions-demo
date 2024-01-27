cb04c22eea7175c1eca07ccf70bf54c0ef1e43f2
kamynz16 <kamynz16@gmail.com>
2024-01-27 17:09:17 -0500

    :green_heart: Fix AWS credentianls in githubactions yaml

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ccamp-github-actions" {
-  ami           = "ami-0c55b159cbfafe1f0" # Specify the AMI ID of the instance
+  ami           = "ami-0c7217cdde317cfec" # Specify the AMI ID of the instance
   instance_type = "t2.micro"              # Specify the instance type
 
   tags = {
